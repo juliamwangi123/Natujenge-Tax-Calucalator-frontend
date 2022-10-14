5b099f22215eec0c43c60717ce4ba661d1e532f3
Jackson Muturi <62056792+jason2000-cpu@users.noreply.github.com>
2022-10-14 20:59:16 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BillManagerComponent } from './components/bill-manager/bill-manager.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillManagerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
