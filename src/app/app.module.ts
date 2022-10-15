78a6ad05cb705d25c95001398e420ee9936be5a6
Jackson Muturi <62056792+jason2000-cpu@users.noreply.github.com>
2022-10-15 16:55:55 +0300

    added bill-manager page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { BillManagerComponent } from './components/bill-manager/bill-manager.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    BillManagerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/bill-manager/bill-manager.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {faTrashCan,faPenToSquare} from '@fortawesome/free-regular-svg-icons'
+
+@Component({
+  selector: 'app-bill-manager',
+  templateUrl: './bill-manager.component.html',
+  styleUrls: ['./bill-manager.component.scss']
+})
+export class BillManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  deleteIcon = faTrashCan;
+  editIcon = faPenToSquare;
+
+}
